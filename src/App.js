8f8aed4f234a5931971a201a80a85a7e94a89dbf
Tyler Waugh <waughphoto@gmail.com>
2022-08-15 23:46:04 -0400

    App + Index Testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import CorgiLogo from './app/assets/img/logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Here comes my portfolio
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Navbar dark color='primary' sticky='top' expand='md'>
+                <Container>
+                    <NavbarBrand href='/'>
+                        <img src={CorgiLogo} alt='corgi logo' />
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
+            Here comes my porfolio! 
+        </div>
+    );
 }
 
-export default App;
+export default App; 
