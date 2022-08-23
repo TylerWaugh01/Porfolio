2ed6b64913feb256e2746e56a60aaf035cf79098
Tyler Waugh <waughphoto@gmail.com>
2022-08-22 22:50:45 -0400

    Test Projects

--- src/app/shared/PROJECTS.js
+++ src/app/shared/PROJECTS.js
@@ -10,5 +10,25 @@ export const PROJECTS = [
         elevator: 'a fictional pet boutique built use bootstrap',
         description:
             'Doggo ipsum bork h*ck woofer boof heckin angery woofer, borkf most angery pupper I have ever seen. heck. Shoob very jealous pupper heckin yapper fluffer, shoober very jealous pupper much ruin diet, borkf doggorino adorable doggo. Puggorino blep many pats clouds heckin very good spot I am bekom fat, big ol tungg clouds wow very biscit porgo.'
+    },
+    {
+        id: 1,
+        name: 'Test 1',
+        image: corgi,
+        website: '',
+        featured: false,
+        elevator: 'Test 1 Elevator',
+        description:
+            'Test 1 description'
+    },
+    {
+        id: 2,
+        name: 'Test 2',
+        image: corgi,
+        website: '',
+        featured: false,
+        elevator: 'Test 2 elevator',
+        description:
+            'Test 2 description'
     }
 ];
