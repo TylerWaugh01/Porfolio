1ac72456847bdf05829fea1be710a2c5faac79e6
Tyler Waugh <waughphoto@gmail.com>
2022-08-22 22:38:39 -0400

    Project Detail/Directory Page

--- src/features/projects/projectsSlice.js
+++ src/features/projects/projectsSlice.js
@@ -3,3 +3,7 @@ import { PROJECTS } from "../../app/shared/PROJECTS";
 export const selectAllProjects = () => {
     return PROJECTS;
 };
+
+export const selectRandomProject = () => {
+    return PROJECTS[Math.floor(PROJECTS.length * Math.random())];
+};
