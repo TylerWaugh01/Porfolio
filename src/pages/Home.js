c5d2ab6582802ffa6bb4bd73ad2270a2a3db467e
Tyler Waugh <waughphoto@gmail.com>
2022-08-27 13:31:04 -0400

    Homepage adjustments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import Tyler from "../app/assets/img/Tyler.png"
 import Butter from "../app/assets/img/Butter.jpeg";
 import { Card, CardBody, CardText, Col, Row, Container, CardTitle } from "reactstrap";
 
@@ -6,14 +7,39 @@ const Home = () => {
     <Container>
       <Row>
         <Col sm={{ size: 'auto', offset: 3 }}>
-          <img src={Butter} alt="Butter" />
+          <img src={Tyler} alt="Tyler" />
         </Col>
         <Col style={{display: "flex" , flexDirection: "column",  justifyContent:'center' }}>
-          <h2 style={{ fontSize: '70px'}}>Tyler Waugh</h2>
-          <h2>Full Stack Developer</h2>
+          <h2 style={{ fontSize: '70px', }}> Tyler Waugh</h2>
+          <h2>  Full Stack Developer</h2>
         </Col>
       </Row>
-      <h2>Who is Tyler?</h2>
+        <Col>
+      <Card
+            className="my-2"
+            color="dark"
+            inverse
+            // style={{ width: "18rem" }}
+          >
+            <CardBody>
+              <CardTitle tag="h5">Skills</CardTitle>
+              <CardText>
+                HTML, CSS, Javascript, Bootstrap, React, React Native, NodeJS,
+                Express, MongoDB
+              </CardText>
+              <CardTitle tag="h5">Education</CardTitle>
+              <CardText>
+                Nucamp Full Stack Web + Mobile Development Bootcamp
+              </CardText>
+              <CardTitle tag="h5">Biography</CardTitle>
+              <CardText>
+                Kansan living in New Jersey. A web developer with a passion for design, user experience, and bringing people together. 
+              </CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      
+      {/* <h2>Who is Tyler?</h2>
       <Row>
         <Col>
           <Card
@@ -62,7 +88,7 @@ const Home = () => {
             </CardBody>
           </Card>
         </Col>
-      </Row>
+      </Row> */}
     </Container>
   );
 };
