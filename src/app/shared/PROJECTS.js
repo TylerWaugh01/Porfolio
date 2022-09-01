f088235f207e5e9987b0464e1ef6d7101b57f116
Tyler Waugh <waughphoto@gmail.com>
2022-09-01 00:01:57 -0400

    Project Changes

--- src/app/shared/PROJECTS.js
+++ src/app/shared/PROJECTS.js
@@ -1,8 +1,19 @@
 import corgi from '../assets/img/corgi.png';
+import Nucamp from '../assets/img/Nucamp.png';
 
 export const PROJECTS = [
     {
-        id: 0,
+        id: 2,
+        name: 'Nucamp Full Stack Site',
+        image: Nucamp,
+        website: '',
+        featured: false,
+        elevator: 'Fictional camp website. Server/Client side',
+        description:
+            'Test 2 description'
+    },
+    {
+        id: 1,
         name: 'Pet Boutique - Bootstrap',
         image: corgi,
         website: '',
@@ -10,16 +21,6 @@ export const PROJECTS = [
         elevator: 'a fictional pet boutique built use bootstrap',
         description:
             'Doggo ipsum bork h*ck woofer boof heckin angery woofer, borkf most angery pupper I have ever seen. heck. Shoob very jealous pupper heckin yapper fluffer, shoober very jealous pupper much ruin diet, borkf doggorino adorable doggo. Puggorino blep many pats clouds heckin very good spot I am bekom fat, big ol tungg clouds wow very biscit porgo.'
-    },
-    {
-        id: 1,
-        name: 'Test 1',
-        image: corgi,
-        website: '',
-        featured: false,
-        elevator: 'Test 1 Elevator',
-        description:
-            'Test 1 description'
     },
     {
         id: 2,
