c59027b470e0f5bedf18a2fd6839793061662c19
Tyler Waugh <waughphoto@gmail.com>
2022-09-24 22:49:00 -0400

    Card Changes

--- src/features/projects/ProjectCard.js
+++ src/features/projects/ProjectCard.js
@@ -5,7 +5,7 @@ const ProjectCard = ({ project }) => {
     const { id, image, name } = project;
     return (
     <Link to={`${id}`}  style={{ textDecoration: 'none', color: 'black'}}>
-        <Card className='my-6' style={{display: "flex" , flexDirection: "column",  justifyContent:'center'}}>
+        <Card className='my-4' style={{display: "flex" , flexDirection: "column",  justifyContent:'center'}}>
            <CardTitle style={{display: "flex" , flexDirection: "row",  justifyContent:'center'}}>{name}</CardTitle>
             <CardImg  src={image} alt={name}/> 
             {/* <CardText>{elevator}</CardText> */}
