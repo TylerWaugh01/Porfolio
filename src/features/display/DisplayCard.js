dc0ca4c64b23843a06d5ed6ac0beb8aaf130f6f4
Tyler Waugh <waughphoto@gmail.com>
2023-01-26 23:42:59 -0500

    Color Changes

--- src/features/display/DisplayCard.js
+++ src/features/display/DisplayCard.js
@@ -4,7 +4,7 @@ const DisplayCard = ({ item }) => {
     const { image, name, description } = item;
     return (
         <Card>
-            <CardImg src={image} alt={name}  style={{marginTop: "50px"}} />
+            <CardImg src={image} alt={name}   />
             <CardBody>
                 <CardTitle>{name}</CardTitle>
                 <CardText>{description}</CardText>
